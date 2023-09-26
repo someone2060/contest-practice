acc4141a07f842378e8d0089dac02dfc7e5e0d30
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2023-09-26 08:25:59 -0600

    Initial commit

---
+++ ccc-2023-s1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string I;
+
+    cin >> I;
+    cout << "Hello World!";
+    cout << endl;
+    cout << "You inputted: " + I;
+    cout << endl;
+    return 1;
+}
