0e79afe8a97a724111768be87918dc887cc0e422
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2023-10-03 15:45:24 -0600

    CCC 2023 J1

---
+++ ccc-2023/j1/ccc-2023-j1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int P, C;
+    cin >> P;
+    cin >> C;
+
+    int out;
+    out = 50*P - 10*C;
+    if (P > C) {
+        out += 500;
+    }
+    cout << out;
+    return 1;
+}
