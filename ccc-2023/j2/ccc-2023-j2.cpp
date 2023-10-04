89e9770c9b9e8853bb9418c3fa22601f9aef92a9
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2023-10-04 13:09:58 -0600

    CCC 2023 J2

---
+++ ccc-2023/j2/ccc-2023-j2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    int out = 0;
+    string in;
+    char inChar;
+    for (int i=0; i<N; i++) {
+        cin >> in;
+        inChar = in.at(0);
+        switch (inChar)
+        {
+        case 'P':
+            out += 1500;
+            break;
+        case 'M':
+            out += 6000;
+            break;
+        case 'S':
+            out += 15500;
+            break;
+        case 'C':
+            out += 40000;
+            break;
+        case 'T':
+            out += 75000;
+            break;
+        case 'H':
+            out += 125000;
+            break;
+        }
+    }
+    cout << out;
+    return 1;
+}
